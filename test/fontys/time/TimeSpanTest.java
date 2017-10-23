560082c89498fd4ca377f8a6f42d9459c63713e3
michel.jacobs <michel.jacobs@indicia.nl>
2017-10-23 18:09:04 +0200

    Continue opdracht 4, Time class fixed.

--- test/fontys/time/TimeSpanTest.java
+++ test/fontys/time/TimeSpanTest.java
@@ -8,46 +8,65 @@ import static org.junit.Assert.*;
 
 public class TimeSpanTest
 {
+    private Time beginTime;
+    private Time endTime;
+    private TimeSpan timeSpan;
+
     @Before
     public void setUp() throws Exception
     {
-        fail();
+        int year = 1995;
+        int month = 8;
+        int day = 17;
+        int hour = 6;
+        int min = 0;
+
+        endTime = new Time(year, month, day, hour, min);
+        beginTime = new Time(year, month, day - 1, hour, min);
+
+        timeSpan = new TimeSpan(beginTime, endTime);
     }
 
     @After
     public void tearDown() throws Exception
     {
-        fail();
     }
 
     @Test
     public void getBeginTime() throws Exception
     {
-        fail();
+        assertEquals(beginTime, timeSpan.getBeginTime());
     }
 
     @Test
     public void getEndTime() throws Exception
     {
-        fail();
+        assertEquals(endTime, timeSpan.getEndTime());
     }
 
     @Test
     public void length() throws Exception
     {
-        fail();
+        int day = 1440;
+
+        assertEquals(day, timeSpan.length());
     }
 
     @Test
     public void setBeginTime() throws Exception
     {
-        fail();
+        Time newBeginTime = (Time) beginTime.plus(1440);
+
+        timeSpan.setBeginTime(newBeginTime);
     }
 
     @Test
     public void setEndTime() throws Exception
     {
-        fail();
+        Time newEndTime = (Time) endTime.plus(1440);
+
+        timeSpan.setBeginTime(newEndTime);
+
     }
 
     @Test
--- test/fontys/time/TimeTest.java
+++ test/fontys/time/TimeTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class TimeTest
 {
 
-    private Time time;
+    private ITime time;
 
     @Before
     public void setUp() throws Exception
@@ -32,57 +32,118 @@ public class TimeTest
     @Test
     public void getDayInWeek() throws Exception
     {
-        assertEquals(time.getDayInWeek(), DayInWeek.FRI );
+
+        //Default is THU (1995-08-17 is Thursday)
+        assertEquals(DayInWeek.THU, time.getDayInWeek());
+
+        //Fri
+        time = new Time(time.getYear(),time.getMonth(), time.getDay() + 1, time.getHours(), time.getMinutes());
+
+        assertEquals(DayInWeek.FRI, time.getDayInWeek());
+
+        // Sat
+        time = new Time(time.getYear(),time.getMonth(), time.getDay() + 1, time.getHours(), time.getMinutes());
+
+        assertEquals(DayInWeek.SAT, time.getDayInWeek());
+
+        //Sun
+        time = new Time(time.getYear(),time.getMonth(), time.getDay() + 1, time.getHours(), time.getMinutes());
+
+        assertEquals(DayInWeek.SUN, time.getDayInWeek());
+
+        //Mon
+        time = new Time(time.getYear(),time.getMonth(), time.getDay() + 1, time.getHours(), time.getMinutes());
+
+        assertEquals(DayInWeek.MON, time.getDayInWeek());
+
+        //Tue
+        time = new Time(time.getYear(),time.getMonth(), time.getDay() + 1, time.getHours(), time.getMinutes());
+
+        assertEquals(DayInWeek.TUE, time.getDayInWeek());
+
+        //Wen
+        time = new Time(time.getYear(),time.getMonth(), time.getDay() + 1, time.getHours(), time.getMinutes());
+
+        assertEquals(DayInWeek.WED, time.getDayInWeek());
 
     }
 
     @Test
     public void getYear() throws Exception
     {
-        assertEquals(time.getYear(), 1995);
+        assertEquals(1995, time.getYear());
     }
 
     @Test
     public void getMonth() throws Exception
     {
+
+        // init test from the before
         assertEquals(time.getMonth(), 8 );
     }
 
     @Test
     public void getDay() throws Exception
     {
-        assertEquals(time.getDay(), 17 );
+        assertEquals(17, time.getDay());
     }
 
     @Test
     public void getHours() throws Exception
     {
-        assertEquals(time.getHours(), 6 );
+        assertEquals(6, time.getHours());
     }
 
     @Test
     public void getMinutes() throws Exception
     {
-        assertEquals(time.getMinutes(), 0 );
+        assertEquals(0, time.getMinutes());
     }
 
     @Test
     public void plus() throws Exception
     {
-//        time.plus();
-        fail();
+        // First test with minuets
+        // Start min is 0
+
+        assertEquals("init", 0, time.getMinutes());
+        time = time.plus(5);
+        assertEquals("adding 5 min, min",5, time.getMinutes());
+        // Add an hour, result should be the same
+        assertEquals("init hours",6, time.getHours());
+        time = time.plus(60);
+        assertEquals("Adding 60 min, min",5, time.getMinutes());
+        assertEquals("Adding 60 min, hour",7, time.getHours());
+
+        // Check if negative works
+
+        time = time.plus(-15);
+        assertEquals("Minus test, minuets",50, time.getMinutes());
+        assertEquals("Minus test, hours",6, time.getHours());
     }
 
     @Test
     public void compareTo() throws Exception
     {
-        fail();
+        Time time2 = new Time((Time) time);
+
+        assertTrue(time2.compareTo(time) == 0);
+
+        time2 = (Time) time.plus(123);
+
+        assertTrue(time2.compareTo(time) != 0);
     }
 
     @Test
     public void difference() throws Exception
     {
-        fail();
+        // Test difference in Minutes
+
+        Time time2 = (Time) time.plus(15);
+
+        assertEquals(15, time2.difference(time));
+
+        assertEquals(0, time2.difference(time2));
     }
 
 }
